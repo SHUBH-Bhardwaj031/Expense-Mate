06edd510adf9d700e0982a21793325c2f7b46505
Shubham Bhardwaj <shubhambhardwaj10oct@gmail.com>
2026-07-24 02:48:46 +0530

    remove netlify cors, add vercel.json for spa routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://expensefront1.netlify.app",
+      "https://expense-mate-sage.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
